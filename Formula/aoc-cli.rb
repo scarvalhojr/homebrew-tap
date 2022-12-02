47c7329cbcdcd57fd80f7ee1236076cd345af24e
Sergio de Carvalho <scarvalhojr@gmail.com>
2022-12-02 20:35:29 +0000

    Update aoc-cli version

--- Formula/aoc-cli.rb
+++ Formula/aoc-cli.rb
@@ -1,8 +1,8 @@
 class AocCli < Formula
   desc "Advent of Code command-line helper tool"
   homepage "https://github.com/scarvalhojr/aoc-cli"
-  url "https://github.com/scarvalhojr/aoc-cli/archive/refs/tags/0.5.1.tar.gz"
-  sha256 "6c9c70462d467ea5e89c4e9a61739349297f4d4954fc9cbf00d3c6194f372ea3"
+  url "https://github.com/scarvalhojr/aoc-cli/archive/refs/tags/0.6.0.tar.gz"
+  sha256 "55f9abfa248063ed892a692ca94251eec827dc47f5c91bc314e9876b28c4c018" 
   license "MIT"
 
   depends_on "rust" => :build
